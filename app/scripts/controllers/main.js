f4174da1b861ede8c4487b8a17082de8f82ceec0
iQhry <ellicul.sent@gmail.com>
2016-10-23 02:05:19 +0800

    fixed order by type and tried extending the controller for detail view

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,7 @@
  * Controller of the pokedexAppApp
  */
 angular.module('pokedexAppApp')
-  .controller('MainCtrl', function ($filter, $scope, PokeStore) {
+  .controller('MainCtrl', function ($filter, $scope, PokeStore, $timeout, $rootScope) {
     
     // init
     //var vm = this;
@@ -26,22 +26,71 @@ angular.module('pokedexAppApp')
     $scope.sortByValue = 'Name';
     $scope.eOrderByType = '';
     $scope.newClass1 = 'btn-primary';
+    $scope.displayMain = true;
+    $scope.displayDetails = false;
     
     //calls to JSON data
     PokeStore.getAll(function(data) {
         $scope.pokemons = data;
-        
+
         for(let i=0; i<data.length; i++) {
-            console.log(data[i]);
+            var getType = [];
+
+            if(data[i].type.length>0) { 
+
+                for(let h=0; h<data[i].type.length; h++) {  
+                    let value = data[i].type[h];
+
+                    if(value==='毒') {
+                        getType.push('Poison');
+                    }else if(value==='地上') {
+                        getType.push('Ground');
+                    }else if(value==='水') {
+                        getType.push('Water');
+                    }else if(value==='飞行') {
+                        getType.push('Flying');
+                    }else if(value==='虫') {
+                        getType.push('Bug');
+                    }else if(value==='炎') {
+                        getType.push('Fire');
+                    }else if(value==='电') {
+                        getType.push('Electric');
+                    }else if(value==='幽灵') {
+                        getType.push('Ghost');
+                    }else if(value==='岩石') {
+                        getType.push('Rock');
+                    }else if(value==='冰') {
+                        getType.push('Ice');
+                    }else if(value==='草') {
+                        getType.push('Grass');
+                    }else if(value==='恶') {
+                        getType.push('Dark');
+                    }else if(value==='龙') {
+                        getType.push('Dragon');
+                    }else if(value==='妖精') {
+                        getType.push('Fairy');
+                    }else if(value==='钢') {
+                        getType.push('Steel');
+                    }else if(value==='一般') {
+                        getType.push('Normal');
+                    }else if(value==='格斗') {
+                        getType.push('Fighting');
+                    }
+
+                    data[i].etype = getType;
+                }
+            }
+
+            //console.log(data[i]); 
             $scope.pokemonData.push(data[i]);
         }
-        
+
         console.log($scope.pokemonData);
     });
     
-    PokeStore.getTypes(function(data) {
-        console.log(data);
-        $scope.types=data;              
+    PokeStore.getTypes(function(data) { 
+        $scope.types=data;    
+        console.log(data);            
     });
     
     $scope.prevPage = function () {
@@ -90,30 +139,35 @@ angular.module('pokedexAppApp')
     $scope.orderByType = function (c, j, e) {
         $scope.eOrderByType = e;
         $scope.orderByTypeValues = [c, j];
-        console.log($scope.eOrderByType);
-        console.log($scope.orderByTypeValues);
     };
     
-    $scope.filterByTypes = function(pm) {
-        var val=($scope.orderByTypeValues.indexOf(pm.type) !== -1);
-        console.log(pm, val);
-        return ($scope.orderByTypeValues.indexOf(pm.type) !== -1);
-    };
-    
-    /*$scope.search = function (pokemonData) {
-        
-        if ($scope.type === undefined || $scope.type.length === 0) {
-            return true;
-        }
-        
-        var found = false;
-        angular.forEach(pokemonData.type, function (pm) {          
-            if (pm.type === parseInt($scope.type)) {
-                found = true;
-            }
+    $scope.triggerChildDetails = function (id) {
+        $timeout(function() {
+            $rootScope.$broadcast('eventBroadcastedName');
+            $scope.title = 'Some Titleasd';
         });
         
-        return found;
-    };*/
+        $scope.$apply(function(){
+            $scope.$broadcast('Sup');
+            $scope.title = 'Some Title';
+        });
+        
+        //$scope.$broadcast('eventBroadcastedName', {myMsg: 'hi children'});
+        console.log('broadcast:', id);
+    };
     
   });
+console.log('passing asdrl');
+ angular.module('pokedexAppApp')
+     .controller('DetailCtrl', ['$scope', function ($scope) {
+          console.log('passing detailCtrl');
+          $scope.$watch('$parent.title', function(newValue, oldValue) {
+              console.log('listen:', oldValue);
+              console.log('new:', newValue);
+              $scope.displayMain = false;
+              $scope.displayDetails = true;
+              $scope.value = 'lucille senturias';
+          });
+  }]);
+
+
